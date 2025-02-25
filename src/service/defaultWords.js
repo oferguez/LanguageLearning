a9063776ec38f7011ef4d8fe999378ec3ae43fdf
Ofer Guez <guez.ofer@gmail.com>
2025-02-25 18:51:54 +0000

    123

--- src/service/defaultWords.js
+++ src/service/defaultWords.js
@@ -4,15 +4,25 @@ export default async function fetchCSV(file) {
     return csvToJSON(text); 
 }
 
+String.prototype.capitalizeFirstLetter = function() {
+    if (!this) return ""; // Handle empty strings safely
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  };  
+
 function csvToJSON(csv) {
     const lines = csv.trim().split("\n");
     const headers = lines[0].split(/[\r,\t]/);
 
-    return lines.slice(1).map(line => {
-        const values = line.split(/[\r,\t]/);
-        return headers.reduce((obj, key, index) => {
-            obj[key] = values[index];
-            return obj;
-        }, {});
-    });
+    try {
+        return lines.slice(1).map(line => {
+            const values = line.split(/[\r,\t]/);
+            return headers.reduce((obj, key, index) => {
+                obj[key] = values[index].capitalizeFirstLetter();
+                return obj;
+            }, {});
+        })
+    } catch (error) {
+        console.error('csvToJSON: error:', error);
+        return [];  
+    }
 }
