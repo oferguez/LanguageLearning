5d58dc8a4f08e8b6f40d1e6477ffcee21fc75ce6
Ofer Guez <guez.ofer@gmail.com>
2025-02-26 09:50:52 +0000

    1

--- src/service/defaultWords.js
+++ src/service/defaultWords.js
@@ -1,28 +1,13 @@
+import Papa from "papaparse";
+
+function csvToJson(csvString) {
+    const results = Papa.parse(csvString, { header: true, skipEmptyLines: true });
+    return results.data; // Returns JSON array
+}
+
 export default async function fetchCSV(file) {
     const response = await fetch(file);
     const text = await response.text();
-    return csvToJSON(text); 
-}
-
-String.prototype.capitalizeFirstLetter = function() {
-    if (!this) return ""; // Handle empty strings safely
-    return this.charAt(0).toUpperCase() + this.slice(1);
-  };  
-
-function csvToJSON(csv) {
-    const lines = csv.trim().split("\n");
-    const headers = lines[0].split(/[\r,\t]/);
-
-    try {
-        return lines.slice(1).map(line => {
-            const values = line.split(/[\r,\t]/);
-            return headers.reduce((obj, key, index) => {
-                obj[key] = values[index].capitalizeFirstLetter();
-                return obj;
-            }, {});
-        })
-    } catch (error) {
-        console.error('csvToJSON: error:', error);
-        return [];  
-    }
+    const wordsArray = csvToJson(text); 
+    return wordsArray;
 }
