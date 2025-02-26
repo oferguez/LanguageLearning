a2665b2217f476a4a3ff43feb4d5c552d4db6f47
Ofer Guez <guez.ofer@gmail.com>
2025-02-26 05:47:29 +0000

    1

--- src/components/Preferences.jsx
+++ src/components/Preferences.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-//import getTranslatedWords from "../service/translationAiFetcher.js";
-import getTranslatedWords from "../service/translate.js";
+import getTranslatedWords from "../service/translationAiFetcher.js";
 
 import fetchCSV from '../service/defaultWords.js';
 
