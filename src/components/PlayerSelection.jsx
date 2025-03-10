264a3f16f0becd92b86482f2f660bb515f41cc43
Ofer Guez <guez.ofer@gmail.com>
2025-03-10 16:40:43 +0000

    wip for mobile screens

--- src/components/PlayerSelection.jsx
+++ src/components/PlayerSelection.jsx
@@ -49,75 +49,49 @@ export const PlayerSelection = ({className, selectedPlayerName, updateSelectedPl
     setSelectedPlayerWorkaround('', false);
   };
 
-  return (
-    <div className={`w-80 md:w-96 lg:w-[40rem] ${className}`}> {/* Set fixed width */}
-        <label className="flex items-center justify-center" htmlFor="player-select">Player Name:</label>
-        <input className="ml-4 pl-4 text-black border border-gray-300 
-            hover:border-4 hover:border-green-500 focus:border-4 
-            focus:border-blue-500 transition-all
-            w-40 md:w-60 lg:w-72"
+return (
+  <div className={`w-74 md:w-96 lg:w-[40rem] ${className}`}> {/* Smaller width on small screens */}
+      <label className="flex items-center justify-center text-sm md:text-base" htmlFor="player-select">
+          Player Name:
+      </label>
+
+      <input 
+          className="ml-2 pl-2 text-black border border-gray-300 
+                     hover:border-2 hover:border-green-500 focus:border-2 
+                     focus:border-blue-500 transition-all
+                     w-32 md:w-48 lg:w-60 text-sm md:text-base"
           list="players-list"
           id="player-select"
-          style={{ width: "10rem" }}
-          value= {isValidObject(selectedPlayer) ? selectedPlayer : "SP is N/A"}
+          value={isValidObject(selectedPlayer) ? selectedPlayer : "SP is N/A"}
           onChange={handlePlayerChange}
           onMouseDown={handleMouseDown} 
-        />
-        <datalist id="players-list">
-          {players.map((player, index) => (
-            <option key={index} value={player}>{player}</option>
-          ))}
-        </datalist>
+      />
 
-        <div className="
-        
-        w-80 md:w-96 lg:w-[rem]
+      <datalist id="players-list">
+        {players.map((player, index) => (
+          <option key={index} value={player}>{player}</option>
+        ))}
+      </datalist>
 
-        ml-4 min-w-[8rem] w-full max-w-[12rem]">
-          {isValidObject(selectedPlayer) && !players.includes(selectedPlayer) && (
-            <button  
-              className="w-full px-4 py-2 bg-red-500 text-white rounded-md shadow-md hover:shadow-lg transition-all" 
-              onClick={handleAddPlayer}
-            >
-              Add Player
-            </button>
-          )}
+      <div className="ml-2 min-w-[4rem] w-full
+                      max-w-[10rem] text-sm md:text-base">
+        {isValidObject(selectedPlayer) && !players.includes(selectedPlayer) && (
+          <button  
+            className={`${selectedPlayer.length === 0 ? 'cursor-not-allowed bg-gray-400' : ''} w-full px-4 py-2 text-white rounded-md `} 
+            onClick={handleAddPlayer}
+          >
+            Add Player
+          </button>
+        )}
 
-          {isValidObject(selectedPlayer) && players.includes(selectedPlayer) && (
-            <button  
-              className="w-full px-4 py-2 bg-red-500 text-white rounded-md shadow-md hover:shadow-lg transition-all" 
-              onClick={handleDeletePlayer}
-            >
-              Delete Player
-            </button>
-          )}
+        {isValidObject(selectedPlayer) && players.includes(selectedPlayer) && (
+          <button  
+            className="w-full px-4 py-2 text-white rounded-md" 
+            onClick={handleDeletePlayer}
+          >
+            Delete Player
+          </button>
+        )}
       </div>
-
-
-    </div>
-
-  );
-};
-
-/*
-<div className="ml-4 min-w-[8rem] w-full max-w-[12rem]">
-  {isValidObject(selectedPlayer) && !players.includes(selectedPlayer) && (
-    <button  
-      className="w-full px-4 py-2 bg-red-500 text-white rounded-md shadow-md hover:shadow-lg transition-all" 
-      onClick={handleAddPlayer}
-    >
-      Add Player
-    </button>
-  )}
-
-  {isValidObject(selectedPlayer) && players.includes(selectedPlayer) && (
-    <button  
-      className="w-full px-4 py-2 bg-red-500 text-white rounded-md shadow-md hover:shadow-lg transition-all" 
-      onClick={handleDeletePlayer}
-    >
-      Delete Player
-    </button>
-  )}
-</div>
-
-*/
+  </div>
+)};
